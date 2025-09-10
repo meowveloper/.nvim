d329b6fedee22f8e688a01fe299e5a34d76ebdaf
meowveloper <meowveloper.dev@gmail.com>
2025-09-11 00:02:05 +0700

    indentation

---
+++ lua/user/init.lua
@@ -0,0 +1,11 @@
+return {
+  options = {
+    opt = {
+        shiftwidth = 4,
+        tabstop = 4,
+        expandtab = true,
+        smartindent = true,
+        format_on_save = false
+    },
+  },
+}
